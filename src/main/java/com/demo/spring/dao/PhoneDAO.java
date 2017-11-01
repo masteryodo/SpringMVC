5e815b79ae70ea64aa98fe1861df24a27c3b1671
masteryodo <fotogosu@gmail.com>
2017-11-01 17:09:49 +0400

    Dao

---
+++ src/main/java/com/demo/spring/dao/PhoneDAO.java
@@ -0,0 +1,11 @@
+package com.demo.spring.dao;
+
+import java.util.List;
+import com.demo.spring.model.Phone;
+public interface PhoneDAO {
+    public void addPhone(Phone p);
+    public void updatePhone(Phone p);
+    public List<Phone> listPhones();
+    public Phone getPhoneById(int id);
+    public void removePhone(int id);
+}
